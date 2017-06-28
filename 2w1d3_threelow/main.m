f5fd4412708d783d95968a8b85442ff8cda8e716
hkimca <harrison.kim.ca@gmail.com>
2017-06-28 17:44:44 -0400

    Initial Commit

---
+++ 2w1d3_threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  2w1d3_threelow
+//
+//  Created by Seantastic31 on 28/06/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
